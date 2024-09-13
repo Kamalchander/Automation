6fca1f20bfc35ce470a9c2bb1a2b978f80f2f3e2
Kamal Chander <kamalchander2002@gmail.com>
2024-09-13 12:57:51 +0530

    Created Login/Logout Automation Code

---
+++ src/main/java/com/automation/script/automation/App.java
@@ -0,0 +1,13 @@
+package com.automation.script.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/automation/script/automation/Loginlogout.java
@@ -0,0 +1,21 @@
+package com.automation.script.automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Loginlogout {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://app.botpenguin.com/authentication");
+		driver.findElement(By.xpath("//*[@id=\"mat-input-0\"]")).click();
+		driver.findElement(By.xpath("//*[@id=\"mat-input-0\"]")).sendKeys("developer@botpenguin.com");
+		driver.findElement(By.xpath("//*[@id=\"mat-input-1\"]")).click();
+		driver.findElement(By.xpath("//*[@id='mat-input-1']")).sendKeys("BotPenguin@2012");
+		driver.findElement(By.xpath("/html/body/app-root/div[1]/div/app-new-signup/div[2]/div/div/form/div[4]/button")).click();
+
+		driver.close();
+	}
+
+}
