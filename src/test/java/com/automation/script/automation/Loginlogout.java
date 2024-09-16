10c32d809c2f1cbc8630c56d5779728a113b7590
Kamal Chander <kamalchander2002@gmail.com>
2024-09-16 19:59:58 +0530

    Ats code enhancement

--- src/test/java/com/automation/script/automation/Loginlogout.java
+++ src/test/java/com/automation/script/automation/Loginlogout.java
@@ -1,23 +1,68 @@
 package com.automation.script.automation;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Loginlogout {
 
-	public static void main(String[] args) throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://stage-hrm.relinns.in/login");
-		driver.manage().window().maximize();
-		driver.findElement(By.name("email")).click();
-		driver.findElement(By.name("email")).sendKeys("admin@relinns.com");
-		driver.findElement(By.name("password")).click();
-		driver.findElement(By.name("password")).sendKeys("12345678");	
-		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/form/button")).click();
+	private WebDriver driver;
+	private WebDriverWait wait;
 
-		driver.wait(2000);
-		driver.close();
+	// Constant for URL, Locators, Fields etc..
+	private static final String URL = "https://stage-hrm.relinns.in/login";
+	private static final String USERNAME = "admin@relinns.com";
+	private static final String PASSWORD = "12345678";
+	private static final By EMAIL_FIELD = By.name("email");
+	private static final By PASSWORD_FIELD = By.name("password");
+	private static final By LOGIN_BUTTON = By.xpath("//*[@id=\"root\"]/div/div/form/button");
+	private static final By PROFILE_SECTION = By.xpath("/html/body/div/div/div[1]/div[2]/div[2]/div/div[2]/div/div[2]");
+	private static final By LOGOUT_BUTTON = By.xpath("//*[@id=\"logout-icon\"]/div[2]/span");
+	private static final By CONFIRM_LOGOUT_BUTTON = By.xpath("/html/body/div[2]/div[3]/div/div/div/div/div/button[2]");
+
+	public void setup() {
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.get(URL);
 	}
 
+	// Login into ATS
+	public void login() {
+		driver.findElement(By.name(EMAIL_FIELD)).click();
+		driver.findElement(By.name(EMAIL_FIELD)).sendKeys(USERNAME);
+		driver.findElement(By.name(PASSWORD_FIELD)).click();
+		driver.findElement(By.name(PASSWORD_FIELD)).sendKeys(PASSWORD);
+		driver.findElement(By.xpath(LOGIN_BUTTON)).click();
+	}
+
+	// Logout from ATS
+	public void logout() {
+		driver.findElement(By.xpath(PROFILE_SECTION)).click();
+		driver.findElement(By.xpath(LOGOUT_BUTTON)).click();
+		driver.findElement(By.xpath(CONFIRM_LOGOUT_BUTTON)).click();
+	}
+
+	// Close the browser
+	public void tearDown() {
+		driver.quit();
+	}
+
+	public static void main(String[] args) {
+		// Create an instance of the class
+		Loginlogout script = new Loginlogout();
+
+		// Call the setup method to initialize and open the browser
+		script.setup();
+
+		// Call the login method to perform the login action
+		script.login();
+
+		// Call the Logout method to perform the logout action
+		script.login();
+	}
 }
