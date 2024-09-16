ccff165983734c760b4e2831db6d904813d0bf9c
Kamal Chander <kamalchander2002@gmail.com>
2024-09-16 11:26:40 +0530

    [Updated] - Ats Login code

--- src/test/java/com/automation/script/automation/Loginlogout.java
+++ src/test/java/com/automation/script/automation/Loginlogout.java
@@ -6,17 +6,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Loginlogout {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://app.botpenguin.com/authentication");
+		driver.get("https://stage-hrm.relinns.in/login");
 		driver.manage().window().maximize();
-		driver.findElement(By.xpath("//*[@id=\"mat-input-0\"]")).click();
-		driver.findElement(By.xpath("//*[@id=\"mat-input-0\"]")).sendKeys("developer@botpenguin.com");
-		driver.findElement(By.xpath("//*[@id=\"mat-input-1\"]")).click();
-		driver.findElement(By.xpath("//*[@id='mat-input-1']")).sendKeys("BotPenguin@2012");
-		driver.findElement(By.xpath("/html/body/app-root/div[1]/div/app-new-signup/div[2]/div/div/form/div[4]/button")).click();
+		driver.findElement(By.name("email")).click();
+		driver.findElement(By.name("email")).sendKeys("admin@relinns.com");
+		driver.findElement(By.name("password")).click();
+		driver.findElement(By.name("password")).sendKeys("12345678");	
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/form/button")).click();
 
-		//driver.close();
+		driver.wait(2000);
+		driver.close();
 	}
 
 }
