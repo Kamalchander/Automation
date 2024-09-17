20a9f11f2b716ce3227169f34e04909cd7b6b5ca
Kamal Chander <kamalchander2002@gmail.com>
2024-09-17 11:23:52 +0530

    [updated] ATS Login Logout Script

--- src/test/java/com/automation/script/automation/Loginlogout.java
+++ src/test/java/com/automation/script/automation/Loginlogout.java
@@ -1,13 +1,11 @@
 package com.automation.script.automation;
 
-import java.sql.Driver;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
